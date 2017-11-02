f3c8f5c6ea0112881a29f28af2ebd68283321152
Will Ballard <wballard@mailframe.net>
2017-11-02 12:31:54 -0500

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools.extension import Extension
 # Package details
 setup(
     name='vectoria',
-    version='0.0.1',
+    version='0.0.2',
     author='Will Ballard',
     author_email='wballard@mailframe.net',
     url='https://github.com/wballard/vectoria',
